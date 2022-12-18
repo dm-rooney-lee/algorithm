064f504b79b5a5d6fce0bcfbd23fe5786d789f5d
dm-rooney-lee <dm.rooney.lee@gmail.com>
2022-12-19 00:02:52 +0900

    add quick sort

---
+++ sort/quick.go
@@ -0,0 +1,28 @@
+package sort
+
+func Quick(arr []int) {
+	quick(arr, 0, len(arr)-1)
+}
+
+func quick(arr []int, low, high int) {
+	if low < high {
+		pi := partition(arr, low, high)
+
+		quick(arr, low, pi-1)
+		quick(arr, pi+1, high)
+	}
+}
+
+func partition(arr []int, low, high int) int {
+	pivot := arr[low]
+	i := low
+	for j := low + 1; j <= high; j++ {
+		if arr[j] <= pivot {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[low], arr[i] = arr[i], arr[low]
+
+	return i
+}
