be5fe6af6d8f898daea58ca68a2727ed93e177ba
dm-rooney-lee <dm.rooney.lee@gmail.com>
2022-12-20 15:00:00 +0900

    fix variable name

--- sort/heap.go
+++ sort/heap.go
@@ -2,28 +2,28 @@ package sort
 
 func Heap(arr []int) {
 	last := len(arr) - 1
-	for i := (last - 1) / 2; i >= 0; i-- {
-		heapify(arr, i, len(arr))
+	for root := (last - 1) / 2; root >= 0; root-- {
+		heapify(arr, root, len(arr))
 	}
 
-	for size := last; size > 0; size-- {
-		arr[0], arr[size] = arr[size], arr[0]
-		heapify(arr, 0, size)
+	for end := last; end > 0; end-- {
+		arr[0], arr[end] = arr[end], arr[0]
+		heapify(arr, 0, end)
 	}
 }
 
-func heapify(arr []int, root, size int) {
+func heapify(arr []int, root, end int) {
 	max := root
 	l, r := root*2+1, root*2+2
-	if l < size && arr[l] > arr[max] {
+	if l < end && arr[l] > arr[max] {
 		max = l
 	}
-	if r < size && arr[r] > arr[max] {
+	if r < end && arr[r] > arr[max] {
 		max = r
 	}
 
 	if max != root {
 		arr[root], arr[max] = arr[max], arr[root]
-		heapify(arr, max, size)
+		heapify(arr, max, end)
 	}
 }
