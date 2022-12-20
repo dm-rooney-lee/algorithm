ed16b9048ead8de73b7b480c227b76682a03c572
dm-rooney-lee <dm.rooney.lee@gmail.com>
2022-12-20 15:19:07 +0900

    add generics to heap sort

--- sort/heap.go
+++ sort/heap.go
@@ -1,29 +1,31 @@
 package sort
 
-func Heap(arr []int) {
-	last := len(arr) - 1
-	for root := (last - 1) / 2; root >= 0; root-- {
-		heapify(arr, root, len(arr))
-	}
+import "golang.org/x/exp/constraints"
 
-	for end := last; end > 0; end-- {
-		arr[0], arr[end] = arr[end], arr[0]
-		heapify(arr, 0, end)
-	}
+func Heap[T constraints.Ordered](arr []T) {
+    last := len(arr) - 1
+    for root := (last - 1) / 2; root >= 0; root-- {
+        heapify(arr, root, len(arr))
+    }
+
+    for end := last; end > 0; end-- {
+        arr[0], arr[end] = arr[end], arr[0]
+        heapify(arr, 0, end)
+    }
 }
 
-func heapify(arr []int, root, end int) {
-	max := root
-	l, r := root*2+1, root*2+2
-	if l < end && arr[l] > arr[max] {
-		max = l
-	}
-	if r < end && arr[r] > arr[max] {
-		max = r
-	}
+func heapify[T constraints.Ordered](arr []T, root, end int) {
+    max := root
+    l, r := root*2+1, root*2+2
+    if l < end && arr[l] > arr[max] {
+        max = l
+    }
+    if r < end && arr[r] > arr[max] {
+        max = r
+    }
 
-	if max != root {
-		arr[root], arr[max] = arr[max], arr[root]
-		heapify(arr, max, end)
-	}
+    if max != root {
+        arr[root], arr[max] = arr[max], arr[root]
+        heapify(arr, max, end)
+    }
 }
